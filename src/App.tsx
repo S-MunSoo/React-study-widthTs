2ea840d929e975ecc41870f871a04759a832b78d
S-MunSoo <godmsoo93@gmail.com>
2022-07-18 21:20:55 +0900

    login vaildator

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./component/Login";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
